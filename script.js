7fb920b0951223985e3b980a22a5ecab85074301
Unknown1407 <118428191+Unknown1407@users.noreply.github.com>
2023-08-19 12:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	let ans = "";
-	for (let i = 0; i < sampleObject.length; i++) {
-		if (hasKey[i].key === sampleObject[i]) {
-			ans += true;
+		if (sampleObject.hasOwnProperty(key)) {
+			return true;
 		}
 		else{
-			ans += false;
+			return false;
 		}
 	}
-	return ans;
-}
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
