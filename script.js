47df629b38af5d631499d33884f189db8a24cfba
Unknown1407 <118428191+Unknown1407@users.noreply.github.com>
2023-08-19 12:29:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let ans = "";
+	for (let i = 0; i < sampleObject.length; i++) {
+		if (hasKey[i].key === sampleObject[i]) {
+			ans += true;
+		}
+		else{
+			ans += false;
+		}
+	}
+	return ans;
 }
 
 // Do not change the code below
